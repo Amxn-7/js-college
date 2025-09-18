5cfcad1dc2e875f9cb0d9d8acee1a67a393ccf3d
Aman Singh <amxnsingh07@gmail.com>
2025-09-18 15:41:36 +0530

    add changes

--- new-project/new-project/src/App.jsx
+++ new-project/new-project/src/App.jsx
@@ -1,70 +1,66 @@
+import { useState } from "react";
+
 function App() {
+  const [city,setcity] = useState('');
+  const [latitude,setlatitude] = useState(0);
+  const [longitude,setlongitude] = useState(0);
+  const [teperature,settemperature] = useState(0);
+  const [sunrise ,setsunrise] = useState(0);
+  const [sunset ,setsunset] = useState(0);
   const getData = async () => {
-    const res = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=london");
+    if(city ==''){
+      alert("Please enter a valid cityname!")
+    }
+    const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
     console.log(res);
     const geoData = await res.json();  
     console.log(geoData);
     const latitude = geoData.results[0].latitude;
     const longitude = geoData.results[0].longitude;
+    setlatitude(geoData.results[0].latitude);
+    setlongitude(geoData.results[0].longitude);
     console.log("Longitude: "+latitude+"Latitude: "+longitude);
 
-    const weatherres = await fetch("https://api.open-meteo.com/v1/forecast?latitude=59.8&longitude=65.4&current_weather=true&hourly=temperature_2m,weather_code");
+    const weatherres = await fetch(
+     ` https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,weather_code
+    `);
     const weatherdata = await weatherres.json();
-    console.log(weatherdata);
-    console.log("Current temperature: "+weatherdata.current_weather.temperature);
-    // console.log("temoperature: "+weatherdata.temperature_);
-    
-    
+    console.log(weatherdata.current_weather.temperature);
+    settemperature(weatherdata.current_weather.temperature);
 
+    const sunRes = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=sunrise,sunset&timezone=auto
+  `);
+  const sunJson = await sunRes.json();
+  console.log(sunJson.daily.sunrise);  // array of sunrise times
+  console.log(sunJson.daily.sunset);   // array of sunset times
+
+  // you can pick sunJson.daily.sunrise[0] or a specific index
+  setsunrise(sunJson.daily.sunrise[0].split("T")[1]);
+  setsunset(sunJson.daily.sunset[0].split("T")[1]);
     
   };
 
   return (
     <>
-      {/* <div className="bg-blue-50 min-h-screen flex">
-
-        <aside className="w-64 bg-white shadow-md p-6 flex flex-col gap-6 justify-between">
-          <div>
-            <h2 className="text-xl font-bold text-blue-600">WI-API</h2>
-            <nav className="flex flex-col gap-4 text-gray-700 mt-6">
-              <a href="#" className="hover:text-blue-600">Home</a>
-              <a href="#" className="hover:text-blue-600">Settings</a>
-              <a href="#" className="hover:text-blue-600">Search</a>
-            </nav>
-          </div>
-          <div>
-            <img src="https://via.placeholder.com/100" alt="profile-img" className="rounded-full"/>
-          </div>
-        </aside>
-
-        <main className="flex-1 flex items-center justify-center p-10">
-          <div className="bg-white w-full max-w-2xl p-10 rounded-xl shadow-md text-center">
-            <p className="text-gray-500 text-2xl mb-8">
-              Hello, <span className="font-semibold text-black">Ready to<br/>Know the weather of your City?</span>
-            </p>
-
-            <div className="flex items-center justify-center gap-4 mb-8">
-              <input id="cityInput" className="border border-blue-600 rounded-md px-4 py-2 text-lg w-2/3" type="text" placeholder="Enter city name" />
-              <button id="getWeatherBtn" onClick={getData} className="bg-black text-white px-4 py-2 rounded-md font-semibold">Get Weather</button>
-            </div>
-
-            <div id="weatherResult" className="bg-gray-100 p-4 rounded-md flex justify-between items-center">
-              <div className="text-left">
-                <div className="flex flex-row items-center">
-                  <p id="city-name-para" className="font-semibold text-sm text-gray-700">City Name, </p>
-                  <p id="country-name-para" className="font-semibold">country Name</p>
-                </div>
-                <p id="latitude" className="text-sm text-gray-500">00:00 GMT+5</p>
-              </div>
-              <div className="flex items-center gap-4">
-                <p id="temprature" className="text-2xl font-bold">25°C</p>
-                <img id="weatherIcon" src="https://i.pinimg.com/736x/7c/44/de/7c44de4d01345d182d239f4ea037f471.jpg" alt="Weather Icon" className="w-10 h-10 object-cover" />
-              </div>
-            </div>
-          </div>
-        </main>
-      </div> */}
+      <div className="flex gap-4 flex-col justify-center items-center h-screen">
+        <div>
+        <h1 className="font-bold text-3xl">Get weather reports</h1>
+        </div>
+        <div className="flex gap-4">
+        <input className="border" placeholder="Enter Cityname" type="text" onChange={(e)=>{
+          setcity(e.target.value)}
+          }/>
       <button className="bg-gray-300 border p-2" onClick={getData}>getdata</button>
+        </div>
+        <div>
+          <p >Latitude: {latitude}</p>
+          <p >Longitude: {longitude}</p>
+          <p >Temperature: {teperature}</p>
+          <p >Sunrise: {sunrise}</p>
+          <p >Sunset: {sunset}</p>
+        </div>
+      </div>
     </>
   )
 }
